f935c981f4948317ba980566b5aec232231f0d0e
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-11-23 21:01:11 -0800

    make function

--- colorUtils.c
+++ colorUtils.c
@@ -1,4 +1,5 @@
-
+#include<stdio.h>
+#include<math.h>
 #include "colorUtils.h"
 
 double rgbIntToFloat(int c) {
@@ -10,8 +11,31 @@ int max(int x, int y, int z) {
   m = m > z ? m : z;
   return m;
 }
+//make min function
+int min(int x, int y, int z){
+      
+}
+//make tograyscalelightness function
+int toGreyScaleLightness(int R, int G, int B){
+
+}
+//make tograyscaleluminosity function
+int toGreyScaleLuminosity(int R, int G, int B){
+
+}
+// make three function toSepia(Red, Green, Blue)
+int toSepiaRed(){
+
+}
+int toSepiaGreen(){
+
+}
+int toSepiaBlue(){
+  
+}
 
 int toGrayScaleAverage(int r, int g, int b) {
   //TODO: test this, it may be wrong!
+
   return (r + g + b) / 3;
 }
