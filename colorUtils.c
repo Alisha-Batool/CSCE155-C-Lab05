0dcc17c340ead6c6219e05afadfd5229edffad1f
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-11-24 00:39:56 -0800

    create function

--- colorUtils.c
+++ colorUtils.c
@@ -15,3 +15,25 @@ int toGrayScaleAverage(int r, int g, int b) {
   //TODO: test this, it may be wrong!
   return (r + g + b) / 3;
 }
+//make min function
+int min(int x, int y, int z){
+      
+}
+//make tograyscalelightness function
+int toGreyScaleLightness(int R, int G, int B){
+
+}
+//make tograyscaleluminosity function
+int toGreyScaleLuminosity(int R, int G, int B){
+
+}
+// make three function toSepia(Red, Green, Blue)
+int toSepiaRed(){
+
+}
+int toSepiaGreen(){
+
+}
+int toSepiaBlue(){
+  
+}
