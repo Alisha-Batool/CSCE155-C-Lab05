051f5fd9e9e7744aaa1577d884874992da1c3ed4
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-11-24 23:24:24 -0800

    create functions and write function body

--- colorUtils.c
+++ colorUtils.c
@@ -9,34 +9,52 @@ int max(int x, int y, int z) {
   int m = x > y ? x : y;
   m = m > z ? m : z;
   return m;
+ 
+}
+//make min function
+int min(int x, int y, int z)
+{
+  int m= x < y ? y : x;
+  m = m < z ? z : m;
+  return m;
 }
 
 int toGrayScaleAverage(int r, int g, int b) {
   //TODO: test this, it may be wrong!
   return (r + g + b) / 3;
 }
-//make min function
-int min(int x, int y, int z){
-      
-}
+
 //make tograyscalelightness function
-int toGreyScaleLightness(int R, int G, int B){
-
+int toGreyScaleLightness(int r, int g, int b)
+{   
+   // int greyScale= (max(r,g,b) + min(r,g,b)) / 2;  
+    //return greyScale; 
+     return (max(r,g,b) + min(r,g,b)) / 2; 
 }
-//make tograyscaleluminosity function
-int toGreyScaleLuminosity(int R, int G, int B){
 
+//make tograyscaleluminosity function
+int toGreyScaleLuminosity(int r, int g, int b)
+{
+  //int greyScale= (0.21*r + 0.72*g + 0.07*b);
+  //return greyScale;
+     return  (0.21*r + 0.72*g + 0.07*b);
 }
 // make three function toSepia(Red, Green, Blue)
-int toSepiaRed()
+int toSepiaRed(int r, int g, int b)
 {
-
+   //int r_new= 0.393*r + 0.769*g + 0.189*b;
+   //return r_new;
+  return (0.393*r + 0.769*g + 0.189*b);
 }
-int toSepiaGreen()
+int toSepiaGreen(int r, int g, int b)
 {
-
+   //int g_new= 0.349*r + 0.686*b + 0.168*b;
+   //return g_new;
+   return (0.349*r + 0.686*b + 0.168*b);
 }
-int toSepiaBlue()
+int toSepiaBlue(int r, int g, int b)
 {
-  
+  //int b_new= 0.72*r + 0.534*g + 0.131*b;
+  //return b_new;
+  return (0.72*r + 0.534*g + 0.131*b);
 }
